6ca802a94d5b1826b30b180b8960ac770baf8633
bharat-029 <bharatbis9oi29@gmail.com>
2025-04-27 14:56:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "api id"
-api_hash = "api hadh"
-bot_token = "bot token"
+api_id = "api id"23664819
+api_hash = "api hash 3853f97c662d5d08cee5f0d07361361e 
+bot_token = "bot token"8092731135:AAGlbGThiXdP7fRtRY_h3UNhOpaj-scPvX4
 auth_users = [8004315740]
 sudo_users = [8004315740]
