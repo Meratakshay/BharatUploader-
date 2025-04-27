bc88039d06c18116cf8b60498828ba1abd067ccb
bharat-029 <bharatbis9oi29@gmail.com>
2025-04-27 13:43:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 api_id = "api id"
 api_hash = "api hadh"
 bot_token = "bot token"
-auth_users = [7890781002]
-sudo_users = [7890781002]
+auth_users = [8004315740,7744345481]
+sudo_users = [8004315740,7744345481]
