ef16ca515aa2a06c106102f0aa9d5820677600a2
Meratakshay <apmerat@gmail.com>
2025-04-28 16:07:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -259,7 +259,7 @@ async def account_login(bot: Client, m: Message):
 
 
 bot.run()
-            if name == "main":
+            if __name__ == "__main__":
     # Start Flask server in a background thread <-- Add this
                 Thread(target=lambda: web_app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))).start()
     
