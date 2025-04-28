2e7af6484527dea46d8fa031c1f4e50cc34c4b33
Meratakshay <apmerat@gmail.com>
2025-04-28 14:40:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "api id"
-api_hash = "api hash"
-bot_token = "bot token"
+api_id = "27743952"
+api_hash = "416ba062bd16c6cf1aa38dd389726023"
+bot_token = "8077516027:AAH_12x8TpnEQwOzmfhXmfqZCIEGOxOkDFo"
 auth_users = [1193564058]
 sudo_users = [1193564058]
